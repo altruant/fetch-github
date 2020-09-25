cfa868272d6008f887b2ca0815d54641f2595f4d
altruant <noellehamberis@gmail.com>
2020-09-25 09:46:07 -0400

    finished base project

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const REPO_URL = 'https://api.github.com/users/altruant/repos'
 const INFO_URL = 'https://api.github.com/users/altruant'
+const ORG_URL = 'https://api.github.com/users/altruant/orgs'
 
 // repo data
+
 const repoData = fetch(REPO_URL)
       .then(response => response.json())
       .then(data => {
-        console.log(data)
+        console.log(data);
         buildRepoTemplate(data);
       });
-const repos = document.getElementById('repo-template').innerHTML
+const repos = document.getElementById('repo-template').innerHTML;
 
 const repoTemplate = Handlebars.compile(repos);
 
@@ -22,11 +24,11 @@ function buildRepoTemplate(repoList) {
 const profileData = fetch(INFO_URL)
   .then(response => response.json())
   .then(data => {
-    console.log(data)
-    buildInfoTemplate(data)
+    console.log(data);
+    buildInfoTemplate(data);
   });
 
-const infos = document.getElementById('info-template').innerHTML
+const infos = document.getElementById('info-template').innerHTML;
 
 const infoTemplate = Handlebars.compile(infos);
 
@@ -34,6 +36,26 @@ function buildInfoTemplate(data) {
   const html = infoTemplate(data);
   document.querySelector('#user-info').innerHTML = html;
 }
+
+// org data
+
+const orgData = fetch(ORG_URL)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    buildOrgTemplate(data);
+  });
+
+const orgs = document.getElementById('org-template').innerHTML;
+
+const orgTemplate = Handlebars.compile(orgs);
+
+function buildOrgTemplate(data) {
+  const html = orgTemplate({orgs: data});
+  document.querySelector('#org-info').innerHTML = html;
+}
+
+// custom helper function
 
 Handlebars.registerHelper('isOne', function(a){
   if(a == 1) {
